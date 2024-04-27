e9d337a9b1dc3746f63d8a0e47c9a82e52f7e3aa
TanzeelFaisal <132434163+TanzeelFaisal@users.noreply.github.com>
2024-04-27 16:29:43 +0500

    added google login

--- Reposter/src/shared/design/signup/signup.design.jsx
+++ Reposter/src/shared/design/signup/signup.design.jsx
@@ -10,6 +10,7 @@ import hidePassword from "@/assets/hide-password.png";
 import GoogleIcon from '@mui/icons-material/Google';
 import { useState } from 'react';
 import axios from 'axios';
+import { useGoogleLogin } from '@react-oauth/google';
 
 const SignupDesignComponent = () => {
     const [showPassword, setShowPassword] = React.useState(false);
@@ -34,9 +35,13 @@ const SignupDesignComponent = () => {
         }));
     };
 
+    const handleTogglePasswordVisibility = () => {
+        setShowPassword(!showPassword);
+    };
+
     const handleSubmit = async () => {
         try {
-            const response = await axios.post('http://localhost:3001/user/add', formData);
+            const response = await axios.post('http://localhost:3001/user/register', formData);
             localStorage.setItem('token', response.data.token)
             if (response.status === 201) {
                 console.log('User registration successful');
@@ -48,8 +53,26 @@ const SignupDesignComponent = () => {
         }
     };
 
-    const handleTogglePasswordVisibility = () => {
-        setShowPassword(!showPassword);
+    const googleLogin = useGoogleLogin({
+        onSuccess: async (codeResponse) => {
+            try {
+                console.log(codeResponse)
+                const response = await axios.post(`http://localhost:3001/user/auth/google`, {
+                    user: codeResponse
+                });
+                const { token } = response.data;
+                localStorage.setItem('accessToken', token);
+                console.log("Token\n", token, " Message:\n", response.data.message)
+            } catch (error) {
+                console.log('Login Error:', error);
+            }
+        },
+        onError: (error) => console.log('Login Failed:', error),
+        scope: ['openid'],
+    });
+
+    const handleGoogleLogin = () => {
+        googleLogin();
     };
 
     return (
@@ -234,7 +257,7 @@ const SignupDesignComponent = () => {
                             <div className='google-btn'>
                                 <materialModules.Button className='google-btn'
                                 startIcon={<GoogleIcon />}
-
+                                onClick={handleGoogleLogin}
                                 >Google</materialModules.Button>
                             </div>
                         </div>
--- Server/controllers/userController.js
+++ Server/controllers/userController.js
@@ -1,10 +1,8 @@
 const User = require('../models/userModel')
 const bcrypt = require('bcrypt');
 const jwtToken = require('../jwtToken');
-
-const get = (req, res) => {
-    res.send("User page")
-}
+const axios = require('axios');
+const GOOGLE_SIGN_IN_PWD = 'Google_Sign_In_$58291&#10_($)231'
 
 const addUser = async (req, res) => {
     try {
@@ -44,7 +42,13 @@ const loginUser = async (req, res) => {
         const existingUser = await User.findOne({ where: { emailAddress } });
         
         if (existingUser) {
-            const isPasswordMatch = await bcrypt.compare(password, existingUser.password);
+            let isPasswordMatch = await bcrypt.compare(GOOGLE_SIGN_IN_PWD, existingUser.password);
+            
+            if (isPasswordMatch) {
+                return res.status(401).json({ error: 'Account signed in with Google' });
+            }
+
+            isPasswordMatch = await bcrypt.compare(password, existingUser.password);
 
             if (isPasswordMatch) {
                 const token = jwtToken.genToken(existingUser.id)
@@ -62,8 +66,44 @@ const loginUser = async (req, res) => {
     }
 }
 
+const googleLogin = async (req, res) => {
+    accessToken = req.body.user.access_token
+    try {
+        const url = "https://www.googleapis.com/oauth2/v1/userinfo";
+        const headers = {
+            "Authorization": `Bearer ${accessToken}`,
+            "Accept": "application/json"
+        };
+        const response = await axios.get(url, { headers });
+        emailAddress = response.data.email
+        
+        const existingUser = await User.findOne({ where: { emailAddress } });
+        if (existingUser) {
+            
+            const token = jwtToken.genToken(existingUser.id)
+
+            return res.status(200).json({ token, message: 'Login successful' });
+        } else {
+            const hashedPassword = await bcrypt.hash(GOOGLE_SIGN_IN_PWD, 10);
+            const newUser = await User.create({
+                firstName : response.data.given_name,
+                lastName: response.data.family_name,
+                emailAddress: response.data.email,
+                password: hashedPassword
+            });
+    
+            const token = jwtToken.genToken(newUser.id)
+    
+            return res.status(201).json({ token, message: 'Registration successful'});
+        }
+    } catch (error) {
+        console.error('Error fetching user info:', error);
+        throw error;
+    }
+}
+
 module.exports = {
-    get : get,
     addUser : addUser,
     loginUser : loginUser,
+    googleLogin: googleLogin
 }
